0b3bfa38fe473b9634d2c80846dd0d54fb0fa6a0
wangyuxiong <15536066999@163.com>
2025-02-12 15:07:44 +0800

    first commit

---
+++ selenium/src/main/java/org/example/SeleniumTest.java
@@ -0,0 +1,40 @@
+package org.example;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class SeleniumTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        // 自动管理 ChromeDriver
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testGoogleSearch() {
+        driver.get("https://www.google.com");
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium WebDriver");
+        searchBox.submit();
+
+        String title = driver.getTitle();
+        assertEquals(true, title.contains("Selenium WebDriver"));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
